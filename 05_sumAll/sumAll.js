3531f6428b38dbd0e56617cbaf23b5ac3402f9d7
DAlexFunk <alexdagreat04@gmail.com>
2024-09-02 11:13:17 -0400

    Finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(beginning, end) {
+    if (beginning < 0 || end < 0) {
+        return "ERROR";
+    } else if (beginning % 1 !== 0 || end % 1 !== 0) {
+        return "ERROR";
+    } else if (typeof beginning !== "number" || typeof end !== "number") {
+        return "ERROR";
+    }
 
+    if (beginning > end) {
+        let extra = beginning;
+        beginning = end;
+        end = extra;
+    }
+
+    let sum = 0;
+    for (let i = beginning; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
